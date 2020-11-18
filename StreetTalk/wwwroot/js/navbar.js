2a26324cd707617bccffa94c7ee27f9926871fa0
19097530 <19097530@student.hhs.nl>
2020-11-18 10:33:02 +0100

    Added basic navbar

---
+++ StreetTalk/wwwroot/js/navbar.js
@@ -0,0 +1,7 @@
+﻿window.addEventListener("load", () => {
+    document.querySelectorAll(".st-nav-expand").forEach((el) => {
+        el.addEventListener("click", () => {
+            document.querySelector(".st-navbar").classList.toggle("expanded");
+        })
+    })
+})
