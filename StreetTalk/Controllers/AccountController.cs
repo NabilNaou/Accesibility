f87d4ac81cf431dc51bd301b0897f4c764c3687e
19097530 <19097530@student.hhs.nl>
2020-11-18 14:36:16 +0100

    Added email verification views

--- StreetTalk/Controllers/AccountController.cs
+++ StreetTalk/Controllers/AccountController.cs
@@ -22,7 +22,20 @@ namespace StreetTalk.Controllers
                 return View(user);
             
             //TODO: Add user to database
-            return RedirectToAction("Registreren");
+            
+            return RedirectToAction("VerifieerEmail");
+        }
+
+        public IActionResult VerifieerEmail(string code)
+        {
+            //TODO: Als er geen gebruiker is ingelogt, redirect naar login
+
+            if (code == null)
+                return View();
+
+            //TODO: Verifieer code met de code in de database
+            
+            return View("VerifieerEmailSucess");;
         }
     }
 }
