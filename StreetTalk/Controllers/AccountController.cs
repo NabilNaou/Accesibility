b776d76791448d86d5e1a5a669d37c7174c38edf
19097530 <19097530@student.hhs.nl>
2020-11-18 13:54:15 +0100

    Added register form

---
+++ StreetTalk/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StreetTalk.Models;
+
+namespace StreetTalk.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Registreren");
+        }
+        
+        public IActionResult Registreren()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registreren(User user)
+        {
+            if(!ModelState.IsValid)
+                return View(user);
+            
+            //TODO: Add user to database
+            return RedirectToAction("Registreren");
+        }
+    }
+}
