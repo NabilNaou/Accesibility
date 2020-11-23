646bfa42bffb60173548b4cf45ebf1a30cb39b46
19097530 <19097530@student.hhs.nl>
2020-11-23 21:54:19 +0100

    Uppdated user model

--- StreetTalk/Models/User.cs
+++ StreetTalk/Models/User.cs
@@ -1,20 +1,39 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StreetTalk.Models
 {
     public class User
     {
-        public int? id { get; set; }
+        public int id { get; set; }
         
         [Display(Name = "Email adres")]
         public string email { get; set; }
         
+        public bool emailConfirmed { get; set; }
+        
         [Display(Name = "Wachtwoord")]
-        public string password { get; set; }
+        public string passwordHash { get; set; }
         
         [NotMapped]
         [Display(Name = "Wachtwoord herhaald")]
         public string confirmPassword { get; set; }
+        
+        public DateTime? createdAt { get; set; }
+        
+        public DateTime? modifiedAt { get; set; }
+        
+        public DateTime? lockoutEndTime { get; set; }
+        
+        public bool lockoutEnabled { get; set; }
+        
+        public int accessFailedCount { get; set; }
+
+        public User()
+        {
+            createdAt = DateTime.UtcNow;
+            modifiedAt = DateTime.UtcNow;
+        }
     }
 }
