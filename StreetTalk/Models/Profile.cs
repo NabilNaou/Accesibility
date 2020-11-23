55be6bb2d30288aa5f32dc82207c31c6ec66c06d
19097530 <19097530@student.hhs.nl>
2020-11-23 22:09:38 +0100

    Added profile model

---
+++ StreetTalk/Models/Profile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StreetTalk.Models
+{
+    public class Profile : Timestamped
+    {
+        public int id { get; set; }
+        
+        [StringLength(45)]
+        public string? firstName { get; set; }
+        
+        [StringLength(45)]
+        public string? lastName { get; set; }
+        
+        [DataType(DataType.Date)]
+        [Column(TypeName = "Date")]
+        public DateTime? dateOfBirth { get; set; }
+        
+        [StringLength(64)]
+        public string? city { get; set; }
+        
+        [StringLength(64)]
+        public string? street { get; set; }
+        
+        public int? houseNumber { get; set; }
+        
+        [StringLength(5)]
+        public string? houseNumberAddition { get; set; }
+    }
+}
