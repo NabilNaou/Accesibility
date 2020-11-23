5d7d562a5fbb801c7d846f08f67527134a6d0010
19097530 <19097530@student.hhs.nl>
2020-11-23 22:20:41 +0100

    Added comment model

---
+++ StreetTalk/Models/Comment.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StreetTalk.Models
+{
+    public class Comment : Timestamped
+    {
+        public int id { get; set; }
+        
+        [StringLength(600)]
+        public string content { get; set; }
+    }
+}
